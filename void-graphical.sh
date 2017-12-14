b67b7a124035efb3433b0c57af8d347bab05a9da
maiwaifu <34530590+maiwaifu@users.noreply.github.com>
2017-12-14 04:24:45 +0000

    Update void-graphical.sh

--- void-graphical.sh
+++ void-graphical.sh
@@ -2,7 +2,7 @@
 xbps-install -Syuv
 
 echo "adding user anon, add password after."
-#useradd -m -s /bin/bash -U -G wheel,users,audio,video,cdrom,input anon
+useradd -m -s /bin/bash -U -G wheel,users,audio,video,cdrom,input anon
 
 echo "installing xorg now"
 xbps-install -Sy xorg-minimal xinit xterm xorg-fonts xf86-input-synaptics xf86-video-intel 
@@ -11,7 +11,7 @@ echo "installing i3"
 xbps-install -Sy i3-gaps 
 
 echo "some extra software"
-xbps-install -Sy git rxvt-unicode ranger cmus cmus-faad cmus-ffmpeg cmus-flac cmus-libao cmus-modplug cmus-mpc cmus-oss cmus-pulseaudio cmus-wavpack compton lxappearance icecat feh polybar wpa_gui transmission transmission-gt unzip w3m w3m-img mpv ImageMagick
+xbps-install -Sy git scrot rxvt-unicode ranger cmus cmus-faad cmus-ffmpeg cmus-flac cmus-libao cmus-modplug cmus-mpc cmus-oss cmus-pulseaudio cmus-wavpack compton lxappearance icecat feh polybar wpa_gui transmission transmission-gt unzip w3m w3m-img mpv ImageMagick
 
 echo "now for some fonts"
 xbps-install -Sy font-awesome  ipafont-fonts-otf noto-fonts-ttf font-unifont-bdf 
